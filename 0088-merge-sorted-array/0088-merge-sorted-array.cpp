f9923c13cb706f63d7711ecb428bf9c88a9cb1df
Akshit Sharma <akshitsharmacodes@gmail.com>
2026-08-25 01:40:27 +0530

    Time: 0 ms (100%), Space: 12.4 MB (76.03%) - took help needs revision

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,16 +1,55 @@
 class Solution {
+private:
+    void swapIfGreater(vector<int>& nums1, int ind1,
+                       vector<int>& nums2, int ind2) {
+        if (nums1[ind1] > nums2[ind2]) {
+            swap(nums1[ind1], nums2[ind2]);
+        }
+    }
+
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i = m - 1;      // last valid element in nums1
-        int j = n - 1;      // last element in nums2
-        int k = m + n - 1;  // last position in nums1 (merged)
 
-        while (j >= 0) {
-            if (i >= 0 && nums1[i] > nums2[j]) {
-                nums1[k--] = nums1[i--];
-            } else {
-                nums1[k--] = nums2[j--];
+        int len = m + n;
+
+        int gap = (len / 2) + (len % 2);
+
+        while (gap > 0) {
+
+            int left = 0;
+            int right = left + gap;
+
+            while (right < len) {
+
+                if (left < m && right < m) {
+                    if (nums1[left] > nums1[right]) {
+                        swap(nums1[left], nums1[right]);
+                    }
+                }
+
+                else if (left < m && right >= m) {
+                    swapIfGreater(nums1, left,
+                                  nums2, right - m);
+                }
+
+                else {
+                    if (nums2[left - m] > nums2[right - m]) {
+                        swap(nums2[left - m], nums2[right - m]);
+                    }
+                }
+
+                left++;
+                right++;
             }
+
+            if (gap == 1)
+                gap = 0;
+            else
+                gap = (gap / 2) + (gap % 2);
+        }
+
+        for (int i = 0; i < n; i++) {
+            nums1[m + i] = nums2[i];
         }
     }
 };
